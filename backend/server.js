2489ee20b0ee44419855f906a7e4654096a2bf56
Namit Malasi <namitmalasi@gmail.com>
2026-03-12 13:15:52 +0530

    adding auth routes and auth middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
