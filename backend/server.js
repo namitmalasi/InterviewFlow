21a05ca4a6e31260cbdb5be9c2615d6473237a1d
Namit Malasi <namitmalasi@gmail.com>
2026-03-20 13:56:51 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,11 @@ import offerRoutes from "./routes/offerRoutes.js";
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://interviewflow-1.onrender.com/", credentials: true }),
+);
 
 app.get("/", (req, res) => {
   res.send("Server running");
